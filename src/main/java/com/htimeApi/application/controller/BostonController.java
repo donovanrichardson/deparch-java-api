e2c56ce222fac20fd2e90546423039976ad8d1fa
donovanrichardson <donovanspqa@gmail.com>
2020-01-15 20:36:39 -0500

    woo-hoo, the port fix worked! now for the actual api functionality

--- src/main/java/com/htimeApi/application/controller/BostonController.java
+++ src/main/java/com/htimeApi/application/controller/BostonController.java
@@ -39,39 +39,34 @@ public class BostonController {
     @RequestMapping("/routes")
     @ResponseBody
     public List<Map<String, Object>> routes(@RequestParam(value = "name", required = false) String name ) {
-//
-//        DSLContext dsl;
-//        try{
-//            dsl = this.connect();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            List error = new ArrayList<Map<String, Object>>();
-//            Map<String, Object> info = new HashMap();
-//            info.put("error", "unable to connect to database");
-//            error.add(info);
-//            return error;
-//        }
-//
-//        try{
-//            this.feedVersion = dsl.select(FEED.LATEST).from(FEED).where(FEED.ID.eq(this.feedId)).fetchOne(FEED.LATEST);
-//
-//            if (name != null){
-//                return dsl.selectFrom(ROUTE).where(ROUTE.DEFAULT_NAME.like("%"+name+"%")).and(ROUTE.FEED_VERSION.eq(this.feedVersion)).fetchMaps();
-//
-//            }else{
-//                return dsl.selectFrom(ROUTE).where(ROUTE.FEED_VERSION.eq(this.feedVersion)).fetchMaps();
-//            }
-//
-//        }catch(Exception e){
-//            throw e;
-//        }finally{
-//            dsl.close();
-//        }
-        List error = new ArrayList<Map<String, Object>>();
+
+        DSLContext dsl;
+        try{
+            dsl = this.connect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            List error = new ArrayList<Map<String, Object>>();
             Map<String, Object> info = new HashMap();
             info.put("error", "unable to connect to database");
             error.add(info);
-        return error;
+            return error;
+        }
+
+        try{
+            this.feedVersion = dsl.select(FEED.LATEST).from(FEED).where(FEED.ID.eq(this.feedId)).fetchOne(FEED.LATEST);
+
+            if (name != null){
+                return dsl.selectFrom(ROUTE).where(ROUTE.DEFAULT_NAME.like("%"+name+"%")).and(ROUTE.FEED_VERSION.eq(this.feedVersion)).fetchMaps();
+
+            }else{
+                return dsl.selectFrom(ROUTE).where(ROUTE.FEED_VERSION.eq(this.feedVersion)).fetchMaps();
+            }
+
+        }catch(Exception e){
+            throw e;
+        }finally{
+            dsl.close();
+        }
     }
 
     //todo too much copying and pasting
@@ -80,129 +75,116 @@ public class BostonController {
     @RequestMapping("/stops")
     @ResponseBody
     public List<Map<String, Object>> stops(@RequestParam(value= "routeId", required = false) String routeId, @RequestParam(value = "name", required = false) String name ) {
-//        DSLContext dsl;
-//        try{
-//            dsl = this.connect();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            List error = new ArrayList<Map<String, Object>>();
-//            Map<String, Object> info = new HashMap();
-//            info.put("error", "unable to connect to database");
-//            error.add(info);
-//            return error;
-//        }
-//
-//        try{
-//
-//            this.feedVersion = dsl.select(FEED.LATEST).from(FEED).where(FEED.ID.eq(this.feedId)).fetchOne(FEED.LATEST);
-//
-//            FeedTable ft = new FeedTable(this.feedVersion, dsl);
-//            if (routeId != null){
-//                ft.setRoute(routeId);
-//            }
-//            if (name != null){
-//                return ft.getStopsMaps(name);//todo return more info from this method
-//            }else return ft.getStopsMaps("");
-//        }catch(Exception e){
-//            throw e;
-//        }finally{
-//            dsl.close();
-//        }
-//
-        List error = new ArrayList<Map<String, Object>>();
-        Map<String, Object> info = new HashMap();
-        info.put("error", "unable to connect to database");
-        error.add(info);
-        return error;
+        DSLContext dsl;
+        try{
+            dsl = this.connect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            List error = new ArrayList<Map<String, Object>>();
+            Map<String, Object> info = new HashMap();
+            info.put("error", "unable to connect to database");
+            error.add(info);
+            return error;
+        }
+
+        try{
+
+            this.feedVersion = dsl.select(FEED.LATEST).from(FEED).where(FEED.ID.eq(this.feedId)).fetchOne(FEED.LATEST);
+
+            FeedTable ft = new FeedTable(this.feedVersion, dsl);
+            if (routeId != null){
+                ft.setRoute(routeId);
+            }
+            if (name != null){
+                return ft.getStopsMaps(name);//todo return more info from this method
+            }else return ft.getStopsMaps("");
+        }catch(Exception e){
+            throw e;
+        }finally{
+            dsl.close();
+        }
+
+
     }
 
     @RequestMapping("/dests")
     @ResponseBody
     public List<Map<String, Object>> dests(@RequestParam(value= "origin") String origin, @RequestParam(value= "route", required = false) String routeId, @RequestParam(value = "name", required = false) String name ) {
-//        DSLContext dsl;
-//        try{
-//            dsl = this.connect();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            List error = new ArrayList<Map<String, Object>>();
-//            Map<String, Object> info = new HashMap();
-//            info.put("error", "unable to connect to database");
-//            error.add(info);
-//            return error;
-//        }
-//
-//        try{
-//            this.feedVersion = dsl.select(FEED.LATEST).from(FEED).where(FEED.ID.eq(this.feedId)).fetchOne(FEED.LATEST);
-//
-//            FeedTable ft = new FeedTable(this.feedVersion, dsl);
-//
-//            ft.setOrigin(origin);
-//
-//            if (routeId != null){
-//                ft.setRoute(routeId);
-//            }
-//            if (name != null){
-//                return ft.getDestinationsMaps(name);
-//            } else return ft.getDestinationsMaps("");
-//        }catch(Exception e){
-//            throw e;
-//        }finally{
-//            dsl.close();
-//        }
-        List error = new ArrayList<Map<String, Object>>();
-        Map<String, Object> info = new HashMap();
-        info.put("error", "unable to connect to database");
-        error.add(info);
-        return error;
+        DSLContext dsl;
+        try{
+            dsl = this.connect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            List error = new ArrayList<Map<String, Object>>();
+            Map<String, Object> info = new HashMap();
+            info.put("error", "unable to connect to database");
+            error.add(info);
+            return error;
+        }
+
+        try{
+            this.feedVersion = dsl.select(FEED.LATEST).from(FEED).where(FEED.ID.eq(this.feedId)).fetchOne(FEED.LATEST);
+
+            FeedTable ft = new FeedTable(this.feedVersion, dsl);
+
+            ft.setOrigin(origin);
+
+            if (routeId != null){
+                ft.setRoute(routeId);
+            }
+            if (name != null){
+                return ft.getDestinationsMaps(name);
+            } else return ft.getDestinationsMaps("");
+        }catch(Exception e){
+            throw e;
+        }finally{
+            dsl.close();
+        }
     }
 
     @RequestMapping("/timetable")
     @ResponseBody
     public Map<String,Object> timetable(@RequestParam(value= "origin") String origin, @RequestParam(value= "dest") String dest, @RequestParam(value= "route", required = false) String routeId, @RequestParam(value= "year") int year, @RequestParam(value= "month") int month, @RequestParam(value= "date") int date) {
-//        DSLContext dsl;
-//        try{
-//            dsl = this.connect();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-////            List error = new ArrayList<Map<String, Object>>();
-//            Map<String, Object> info = new HashMap();
-//            info.put("error", "unable to connect to database");
-////            error.add(info);
-//            return info;
-//        }
-//
-//        try{
-//            Map<String,Object> result = new HashMap<>();
-//
-//            this.feedVersion = dsl.select(FEED.LATEST).from(FEED).where(FEED.ID.eq(this.feedId)).fetchOne(FEED.LATEST);
-//
-//            FeedTable ft = new FeedTable(this.feedVersion, dsl);
-//
-//            ft.setOrigin(origin);
-//            ft.setDestination(dest);
-//            ft.setDate(year, month, date);
-//
-//            if (routeId !=  null){
-//                ft.setRoute(routeId);
-//                result.put("route", dsl.selectFrom(ROUTE).where(ROUTE.ROUTE_ID.eq(routeId)).and(ROUTE.FEED_VERSION.eq(this.feedVersion)).fetchOneMap());
-//            }
-//
-//            result.put("origin", dsl.selectFrom(STOP).where(STOP.STOP_ID.eq(origin)).fetchOneMap());
-//            result.put("destination", dsl.selectFrom(STOP).where(STOP.STOP_ID.eq(dest)).fetchOneMap());
-//            result.put("result", ft.getTimetableMaps());
-//            result.put("date", ft.dateString());
-//            return result;
-//            //todo make sure to specify feed_version
-//        }catch(Exception e){
-//            throw e;
-//        }finally{
-//            dsl.close();
-//        }
+        DSLContext dsl;
+        try{
+            dsl = this.connect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+//            List error = new ArrayList<Map<String, Object>>();
+            Map<String, Object> info = new HashMap();
+            info.put("error", "unable to connect to database");
+//            error.add(info);
+            return info;
+        }
 
+        try{
+            Map<String,Object> result = new HashMap<>();
 
-        Map<String, Object> info = new HashMap();
-        info.put("error", "unable to connect to database");
-        return info;
+            this.feedVersion = dsl.select(FEED.LATEST).from(FEED).where(FEED.ID.eq(this.feedId)).fetchOne(FEED.LATEST);
+
+            FeedTable ft = new FeedTable(this.feedVersion, dsl);
+
+            ft.setOrigin(origin);
+            ft.setDestination(dest);
+            ft.setDate(year, month, date);
+
+            if (routeId !=  null){
+                ft.setRoute(routeId);
+                result.put("route", dsl.selectFrom(ROUTE).where(ROUTE.ROUTE_ID.eq(routeId)).and(ROUTE.FEED_VERSION.eq(this.feedVersion)).fetchOneMap());
+            }
+
+            result.put("origin", dsl.selectFrom(STOP).where(STOP.STOP_ID.eq(origin)).fetchOneMap());
+            result.put("destination", dsl.selectFrom(STOP).where(STOP.STOP_ID.eq(dest)).fetchOneMap());
+            result.put("result", ft.getTimetableMaps());
+            result.put("date", ft.dateString());
+            return result;
+            //todo make sure to specify feed_version
+        }catch(Exception e){
+            throw e;
+        }finally{
+            dsl.close();
+        }
+
     }
 
 }
